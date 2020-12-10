dfe929cee90f31f14935168ad2e48900e742dd0c
MarcoB <mrc.bsllt@gmail.com>
2020-12-10 12:59:38 +0100

    Aggiunto il bottone per depennare tutta la lista di cose da fare

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ const app = new Vue (
     el: "#todo_section",
     data: {
       toDoList: ["Fare la spesa", "Pagare le bollette", "Completare l'esercizio Boolean", "Cucinare il pranzo", "Pulire la casa"],
-      inputText: ""
+      inputText: "",
     },
     methods: {
       deleteToDo: function(index) {
         this.toDoList.splice(index, 1);
       },
       addToDo: function() {
+        this.visible = true;
         this.toDoList.push(this.inputText);
         this.inputText = "";
+      },
+      deleteAll: function() {
+        this.toDoList = [];
       }
     }
   }
