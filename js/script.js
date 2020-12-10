5600b463997fe792a25723363ecf336622a378cf
MarcoB <mrc.bsllt@gmail.com>
2020-12-10 11:55:11 +0100

    creato la lista delle cose da fare

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-const app = New Vue (
+const app = new Vue (
   {
-    
+    el: "#todo_section",
+    data: {
+      toDoList: ["Fare la spesa", "Pagare le bollette", "Completare l'esercizio Boolean", "Cucinare il pranzo", "Pulire la casa"],
+      visible: true
+    },
+    methods: {
+      deleteToDo: function() {
+        this.visible = false;
+      }
+    }
   }
 );
